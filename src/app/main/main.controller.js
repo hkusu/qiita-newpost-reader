5b71225b77b55332be6a7488ec26e080d4830cad
Hiroyuki Kusu <kusu0806@gmail.com>
2015-01-22 17:47:52 +0900

    add search by tags

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,23 +4,30 @@ angular.module('qiitaNewpostReader')
   .controller('MainCtrl', function ($scope, $http, $ionicLoading, $ionicModal, $ionicScrollDelegate) {
     $scope.next_page = 1;
     $scope.search_keyword = "";
+    $scope.tag_url = "";
+    $scope.tag_name = "";
+    $scope.user_url = "";
     $scope.message = "";
     $scope.show_infinite = true;
     var api_url = "";
     var PER_PAGE = 20;
 
     // 初期表示は View の infinite-scroll から呼ばれる
-    $scope.load = function(_page, _keyword){
+    $scope.load = function(_page, _keyword, _tag_url, _tag_name, _user_url){
       // Loading... を表示
       $ionicLoading.show({
         template: 'Loading...',
         noBackdrop: true
       });
-      // キーワードありなしで対象APIを変更
-      if (_keyword == ""){
-        api_url = 'https://qiita.com/api/v1/items?';
-      } else {
+      // 対象APIの決定
+      if (_keyword != "") {
         api_url = 'https://qiita.com/api/v1/search?' + 'q=' + _keyword + '&';
+      } else if (_tag_url != "") {
+        api_url = 'https://qiita.com/api/v1/tags/' + _tag_url + '/items?';
+      } else if (_user_url != "") {
+        api_url = 'https://qiita.com/api/v1/users/' + _user_url + '/items?';
+      } else {
+        api_url = 'https://qiita.com/api/v1/items?';
       }
       // APIリクエストを発行
       $http.get(api_url + 'per_page=' + PER_PAGE + '&page=' + _page).success(function(items) {
@@ -37,18 +44,23 @@ angular.module('qiitaNewpostReader')
           }
           // 内容をクリア
           $scope.items= [];
-          // キーワード検索の場合は一覧のスクロールをTOPへ
-          if (_keyword != ""){
-            $ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
-          }
+          // 一覧のスクロールをTOPへ
+          $ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
         }
         // Loading... を隠す
         $ionicLoading.hide();
         // infinite-scroll 欄の表示/非表示
-        if (items.length == PER_PAGE) {
-          $scope.show_infinite = true;
-        } else {
-          $scope.show_infinite = false;
+        $scope.show_infinite = true;
+        if (items.length != PER_PAGE) {
+          // なぜかタグでの検索は指定した件数が返ってこないので対策
+          //$scope.show_infinite = false;
+          if (_tag_url == "") {
+            $scope.show_infinite = false;
+          } else {
+            if (items.length == 0) {
+              $scope.show_infinite = false;
+            }
+          }
         }
         // 既存の内容に結合
         $scope.items = $scope.items.concat(items);
@@ -57,20 +69,27 @@ angular.module('qiitaNewpostReader')
         // ページ下部の infinite-scroll を終了
         $scope.$broadcast('scroll.infiniteScrollComplete');
         // ページ上部にメッセージを表示
-        if (_keyword == ""){
-          $scope.message = ""
-        } else {
+        if (_keyword != "") {
           $scope.message = '"' + _keyword + '"' + ' での検索結果'
+        } else if (_tag_url != "") {
+          $scope.message = '"' + _tag_name + '"' + ' タグの投稿'
+        } else if (_user_url != "") {
+          $scope.message = _user_url + ' の投稿'
+        } else {
+          $scope.message = ""
         }
         // ページ上部のリフレッシュおよび下部の infinite-scroll 向けに view へ値をセット
         $scope.next_page = _page + 1;
         $scope.search_keyword = _keyword;
+        $scope.tag_url = _tag_url;
+        $scope.tag_name = _tag_name;
+        $scope.user_url = _user_url;
       });
     };
 
     // 検索ボタン
     $scope.doSearch = function(_keyword){
-      $scope.load(1, _keyword);
+      $scope.load(1, _keyword, '', '', '');
       $scope.search_modal.hide();
     };
 
